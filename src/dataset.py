8a8dcbf7830a9e1d7b20e9701d5089914ac50cb6
Iqra Asghar <114947104+IQRAASGHAR1999@users.noreply.github.com>
2025-09-25 22:03:39 +0500

    Create dataset.py

---
+++ src/dataset.py
@@ -0,0 +1,9 @@
+# Utility functions for loading datasets, augmentations, etc.
+# Add your custom dataset and preprocessing code here.
+def get_dataloaders(transform, batch_size=32):
+    import torchvision
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+    return trainloader, testloader
